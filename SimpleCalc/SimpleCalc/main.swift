26a7e9cd5f3ec5a0240769ebaebd5ec3084a5945
Sagar Surana <Sagar@D-10-19-74-246.dhcp4.washington.edu>
2018-10-03 15:34:02 -0700

    calc application

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -9,11 +9,63 @@
 import Foundation
 
 public class Calculator {
+    
     public func calculate(_ args: [String]) -> Int {
+        var res: Int = 0
+        let size = args.count;
+        if size == 1 {
+            return 0;
+        }
+        let lastElement = Int(args[size-1])
+        if (lastElement != nil) {
+            let first = (Int(args[0]))!
+            let second = (Int(args[2]))!
+            switch args[1] {
+            case "+":
+                res = first + second
+            case "*":
+                res = first * second
+            case "/":
+                res = first / second
+            case "-":
+                res = first - second
+            default: //modulo operator
+                res = first % second
+            }
+        } else {
+            let first = (Int(args[0]))!
+            switch args[size-1] {
+            case "fact":
+                res = factorial(first)
+            case "avg":
+                res = average(args)
+            default: //count
+                res = args.count - 1
+            }
+        }
+        return res;
     }
     
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
+    }
+    
+    public func factorial(_ param: Int) -> Int {
+        var response = param
+        for i in 1..<param {
+            response = response * (param-i)
+        }
+        return response
+    }
+    
+    public func average(_ arr: [String]) -> Int {
+        var avg: Int;
+        var sum = 0;
+        for i in 0..<(arr.count-1) {
+            sum += (Int(arr[i]))!
+        }
+        avg = sum/(arr.count-1)
+        return avg
     }
 }
 
